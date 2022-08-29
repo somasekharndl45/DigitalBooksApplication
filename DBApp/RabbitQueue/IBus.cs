5d44a8c240ceb5d161e1c1591c8a3db67fa9aa91
somasekhar <somasekharndl45@gmail.com>
2022-08-29 07:23:05 +0530

    Rabbitmq implemented

---
+++ DBApp/RabbitQueue/IBus.cs
@@ -0,0 +1,9 @@
+﻿namespace CommonUtilities.RabbitQueue
+{
+    public interface IBus
+    {
+        Task SendSync<T>(string queue, T message); 
+        Task ReceiveAsync<T>(string queue, Action<T> onMessage);
+
+    }
+}
